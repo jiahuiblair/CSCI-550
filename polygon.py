a239923a20f9f99c3f67d2db3494c61f0fbbc721
Robin Belton <robin.belton@montana.edu>
2020-10-18 12:28:32 -0600

    Update polygon file

--- polygon.py
+++ polygon.py
@@ -15,101 +15,129 @@ iris=pd.DataFrame({'Sepal Length':iristemp[0], 'Sepal Width':iristemp[1],
                  'Petal Length':iristemp[2], 'Petal Width':iristemp[3], 
                  'Species':iristemp[4]})
 
-def sample_rectangles(k,n): #sample k points from n rectangles. Rectangles are formed by x-coordinates and y-coordinates
-   left_boundary = 0
-   sampled_regions=[]
-   bottom_boundary = iris['Sepal Width'].quantile(0)
-   top_boundary = iris['Sepal Width'].quantile(1)
-   poly_regions=np.zeros((k*n, 3))
-   for i in range(n):
-       right_boundary = iris['Sepal Length'].quantile((1+i)/n)
-       region = []
-       for j in range(150):
-           if left_boundary<iris['Sepal Length'][j]<right_boundary and bottom_boundary<iris['Sepal Width'][j]<top_boundary:
-               region.append((iris['Sepal Length'][j],iris['Sepal Width'][j]))
-       sampled_regions.append(random.sample(region, k))
-       left_boundary = right_boundary 
-       x,y = zip(*sampled_regions[i])
-       for m in range(len(x)): #organize regions into an array
-           poly_regions[m+k*i][2]=i
-           poly_regions[m+k*i][0]=x[m]
-           poly_regions[m+k*i][1]=y[m]        
-   return poly_regions
-
-def sample_rectangles_noise(k,n,r): #sample k points from n rectangles, with r noise points Rectangles are formed by x-coordinates and y-coordinates
-   left_boundary = 0
-   sampled_regions=[]
-   bottom_boundary = iris['Sepal Width'].quantile(0)
-   top_boundary = iris['Sepal Width'].quantile(1)
-   poly_regions=np.zeros((k*n+r, 3))
-   for i in range(n):
-       right_boundary = iris['Sepal Length'].quantile((1+i)/n)
-       region = []
-       for j in range(150):
-           if left_boundary<iris['Sepal Length'][j]<right_boundary and bottom_boundary<iris['Sepal Width'][j]<top_boundary:
-               region.append((iris['Sepal Length'][j],iris['Sepal Width'][j]))
-       sampled_regions.append(random.sample(region, k))
-       left_boundary = right_boundary 
-       x,y = zip(*sampled_regions[i])
-       for m in range(len(x)): #organize regions into an array
-           poly_regions[m+k*i][2]=i
-           poly_regions[m+k*i][0]=x[m]
-           poly_regions[m+k*i][1]=y[m]
-   xmin = min(iris['Sepal Length'])
-   xmax = max(iris['Sepal Length'])
-   for i in range(r):
+def SyntheticRectData(k, n, r): 
+    '''
+    Samples k points from n non-overlapping rectangles, and adds r noise points to the data
+    :param k: an integer denoting the number of points to be sampled from each rectangular region
+    :param n: an integer denoting the number of non-overlapping rectangles to form
+    :param r: an integer denoting the number of noise points to add
+    :return: a data frame with X, Y, and Label columns. The label 
+             denotes the rectangle the point was sampled from. A label of 0 
+             means the point is a noise point
+    
+    '''
+    left_boundary = 0
+    bottom_boundary = 0
+    top_boundary = 7
+    poly_regions=np.zeros((k*n+r, 3))
+    for i in range(n):
+        right_boundary = (7/n)*(i+1)
+        for j in range(k):
+            poly_regions[j+k*i][2]=i+1
+            poly_regions[j+k*i][0]=random.uniform(left_boundary,right_boundary-.05)
+            poly_regions[j+k*i][1]=random.uniform(bottom_boundary,top_boundary)
+        left_boundary = right_boundary
+    xmin = 0
+    xmax = 7
+    for i in range(r): #Add noise to data
        if i%2 == 0:
            poly_regions[k*n+i][0]=random.uniform(xmin-2, xmin)
            poly_regions[k*n+i][1]=random.uniform(bottom_boundary-2, bottom_boundary)
-           poly_regions[k*n+i][2]=3
+           poly_regions[k*n+i][2]=0
        else:
            poly_regions[k*n+i][0]=random.uniform(xmax, xmax+2)
            poly_regions[k*n+i][1]=random.uniform(top_boundary, top_boundary+2)
-           poly_regions[k*n+i][2]=3
-   return poly_regions
+           poly_regions[k*n+i][2]=0
+    poly_data = pd.DataFrame({'X':poly_regions[:,0], 'Y':poly_regions[:,1], #convert matrix into a data frame
+                 'Label':poly_regions[:,2]})
+    return poly_data       
+       
+# Example of generating synthetic data
+synth = SyntheticRectData(30,3,10)
+plt.plot(synth['X'][0:29], synth['Y'][0:29], 'ro', #plot results
+         synth['X'][30:59], synth['Y'][30:59], 'bo', 
+         synth['X'][60:89], synth['Y'][60:89], 'go',
+         synth['X'][90:99], synth['Y'][90:99], 'yo')
+plt.show()       
 
-# example with plot results
-poly_points = sample_rectangles(30, 3)
-plt.plot(poly_points[0:29, 0], poly_points[0:29, 1], 'ro', 
-         poly_points[30:59, 0], poly_points[30:59, 1], 'bo', 
-         poly_points[60:89, 0], poly_points[60:89, 1], 'go' )
+def GenerateRectanglePartition(D,k,n): #not needed for iris data since we know ground truth, but could be useful for other data
+    '''
+    Samples k points from n non-overlapping rectangles of a data set. 
+    :param D: A 2D data frame
+    :param k: an integer denoting the number of points to be sampled
+    :param n: an integer denoting the number of rectangles to form
+    :return: a data frame with X, Y, and Label columns. The label 
+             denotes the rectangle the point was sampled from.     
+    '''
+    left_boundary = 0
+    sampled_regions=[]
+    poly_regions=np.zeros((k*n, 3))
+    for i in range(n):
+       right_boundary = D.iloc[:,0].quantile((1+i)/n)
+       region = []
+       for j in range(len(D.iloc[:,0])): #partition points into rectangular regions
+           if left_boundary<D.iloc[j,0]<right_boundary: 
+               region.append((D.iloc[j,0],D.iloc[j,1]))
+       sampled_regions.append(random.sample(region, k)) #sample points from each rectangle
+       left_boundary = right_boundary 
+       x,y = zip(*sampled_regions[i])
+       for m in range(len(x)): #organize regions into a matrix
+           poly_regions[m+k*i][2]=i+1
+           poly_regions[m+k*i][0]=x[m]
+           poly_regions[m+k*i][1]=y[m]
+    poly_data = pd.DataFrame({'X':poly_regions[:,0], 'Y':poly_regions[:,1], #convert matrix into a data frame
+                 'Label':poly_regions[:,2]})
+    return poly_data
+
+# Example of generating rectangular partitions of iris data
+D = iris[['Sepal Length', 'Sepal Width']]
+rect_points= GenerateRectanglePartition(D, 30, 3)
+plt.plot(rect_points['X'][0:29], rect_points['Y'][0:29], 'ro', #plot results
+         rect_points['X'][30:59], rect_points['Y'][30:59], 'bo', 
+         rect_points['X'][60:89], rect_points['Y'][60:89], 'go')
 plt.show()
 
-# example with noise
-poly_points_noise = sample_rectangles_noise(30, 3, 10)
-plt.plot(poly_points_noise[0:29, 0], poly_points_noise[0:29, 1], 'ro', 
-         poly_points_noise[30:59, 0], poly_points_noise[30:59, 1], 'bo', 
-         poly_points_noise[60:89, 0], poly_points_noise[60:89, 1], 'go',
-         poly_points_noise[90:99,0], poly_points_noise[90:99,1], 'yo')
-plt.show()
 
-#define purity function - this assumes we have 3 clusters. Could generalize this more to take in an arbitrary 
-# amount of clusters and applying some sort of permutation function on sets
-def purity(D, C, k, r): #input is a dataframe, D and a dataframe with the clustered data, C, sample k points
-    ground_truth = sample_rectangles(k,3,r)
+def Purity(D, C, n): 
+    '''
+    Computes purity based from 2D data clustered from 3 regions compared to given partition data
+    :param D: A data frame with attributes, X, Y, Labels. The labels are taken to be the ground truth
+    :param C: A data frame of the clustered data with attributes, X, Y, Labels
+    :param n: number of points in Data set
+    :return: The purity of C given that D is the ground truth.
+    
+    '''
+    ground_truth = D.as_matrix()
     Cmat = C.as_matrix()
     
-    A = {(ground_truth[i,0],ground_truth[i,1]) for i in range(k)}
-    B = {(ground_truth[i,0],ground_truth[i,1]) for i in range(k,2*k)}
-    C = {(ground_truth[i,0],ground_truth[i,1]) for i in range(2*k,3*k)}
+    A = {(ground_truth[i,0],ground_truth[i,1]) for i in range(len(ground_truth[:,1])) if ground_truth[i,2] == 1}
+    B = {(ground_truth[i,0],ground_truth[i,1]) for i in range(len(ground_truth[:,1])) if ground_truth[i,2] == 2}
+    C = {(ground_truth[i,0],ground_truth[i,1]) for i in range(len(ground_truth[:,1])) if ground_truth[i,2] == 3}
     
-    Q = {(Cmat[i,0],Cmat[i,1]) for i in range(len(Cmat[:,1])) if Cmat[i,2] == 0}
-    R = {(Cmat[i,0],Cmat[i,1]) for i in range(len(Cmat[:,1])) if Cmat[i,2] == 1}
-    S = {(Cmat[i,0],Cmat[i,1]) for i in range(len(Cmat[:,1])) if Cmat[i,2] == 2}
+    Q = {(Cmat[i,0],Cmat[i,1]) for i in range(len(Cmat[:,1])) if Cmat[i,2] == 1}
+    R = {(Cmat[i,0],Cmat[i,1]) for i in range(len(Cmat[:,1])) if Cmat[i,2] == 2}
+    S = {(Cmat[i,0],Cmat[i,1]) for i in range(len(Cmat[:,1])) if Cmat[i,2] == 3}
     
     bestAmatch = max(len(A.intersection(Q)), len(A.intersection(R)), len(A.intersection(S)))
     bestBmatch = max(len(B.intersection(Q)), len(B.intersection(R)), len(B.intersection(S)))
     bestCmatch = max(len(C.intersection(Q)), len(C.intersection(R)), len(C.intersection(S)))
     
-    #Don't think I need what is below but am just commenting it out for now in case I do
-    #score=[]
-    #score.append(len(A.intersection(Q))+len(B.intersection(R))+len(C.intersection(S)))
-    #score.append(len(A.intersection(Q))+len(B.intersection(S))+len(C.intersection(R)))
-    #score.append(len(A.intersection(R))+len(B.intersection(Q))+len(C.intersection(S)))
-    #score.append(len(A.intersection(R))+len(B.intersection(S))+len(C.intersection(R)))
-    #score.append(len(A.intersection(S))+len(B.intersection(Q))+len(C.intersection(R)))
-    #score.append(len(A.intersection(S))+len(B.intersection(R))+len(C.intersection(Q)))
-    
-    return((bestAmatch+bestBmatch+bestCmatch)/(150+r))
-    
+    return((bestAmatch+bestBmatch+bestCmatch)/(n))
+ 
+# Make ground truth data of Iris Data
+D = iris[['Sepal Length', 'Sepal Width', 'Species']]
+ground_truth = np.zeros((len(D.iloc[:,0]),3))
+ground_truth[:,0] = D.iloc[:,0]
+ground_truth[:,1] = D.iloc[:,1]
+for i in range(len(D.iloc[:,0])):
+    if D.iloc[i,2]=='Iris-setosa':
+        ground_truth[i,2] = 1
+    elif D.iloc[i,2]=='Iris-versicolor':
+        ground_truth[i,2] = 2
+    else:
+        ground_truth[i,2] = 3
+groundtruth = pd.DataFrame({'X':ground_truth[:,0], 'Y':ground_truth[:,1], 
+                 'Label':ground_truth[:,2]})       
+# Compute purity with Kmeans output, km (need to run k-means clustering function to get km)
+Purity(groundtruth, km, 150)
     
