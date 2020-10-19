1c6eabdb6fd3c01bc48a8029cc1afb40e7b3dba8
Daniel Laden <Dthomasladen@Gmail.com>
2020-10-19 15:01:54 -0600

    Fixed the minpts problem

--- dbscan.py
+++ dbscan.py
@@ -28,17 +28,22 @@ temp["label"]=np.nan
 def FindPointsInRange(data, p, eps):
 	neighbours = []
 	attri_len = len(p)-1
+	#print(attri_len)
 	for index, q in data.iterrows():
-		dist = LA.norm(p[:attri_len]-q[:attri_len])#this takes the L2 norm of the attributes giving us it's euclidian distance.
-		#print(dist)
+		dist = LA.norm(p[:attri_len]-q[:attri_len])#NOTE change for dataframe
+		# print(dist)
 		if dist <= eps and dist != 0: #if it is below our threshold distance eps and isn't itself add it to the list.
 			neighbours.append(q)
+	# 		print("Here")
+	# print(len(neighbours))
+	# print(neighbours)
 	return neighbours
 
 def dbscan(data, eps, minPts):
 	Cluster_num = 0
 	for index, point in data.iterrows():
 		if not math.isnan(point[2]): #point already has a label from explorative steps
+			#print("point")
 			continue
 		neighbours = FindPointsInRange(data, point, eps) #find all closest neighbours
 		if len(neighbours) < minPts: #if point doesn't have enough neighours label it as noise
@@ -48,25 +53,22 @@ def dbscan(data, eps, minPts):
 		point[2] = Cluster_num
 		set_of_neighbours = neighbours
 		#print(len(set_of_neighbours))
-		for n_point in neighbours:
+		for n_point in set_of_neighbours:
 			if n_point[2] == -1: #point isn't a noise point, it's part of a cluster
 				n_point[2] = Cluster_num
-			if not math.isnan(point[2]): #point already has a label from explorative steps
+			if not math.isnan(n_point[2]): #point already has a label from explorative steps
 				continue
 			n_point[2] = Cluster_num
 			neighbours = FindPointsInRange(data, n_point, eps)
 			if len(neighbours) >= minPts:
-				set_of_neighbours = list(set().union(set_of_neighbours, neighbours)) #add any additional points that meet the distance requirement
+				set_of_neighbours = set_of_neighbours+neighbours #This might be the union of the two but I'm unsure, it seems to work but this might cause larger sets to take forever
 			#print(len(set_of_neighbours))
 	return data
 
-
+			#db, e value, minpts
 db = dbscan(temp, 0.3, 10)
 
 print(db)
-
-
-
 
 
 ###################################
